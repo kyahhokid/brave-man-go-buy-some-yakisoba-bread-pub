ff34b9b76050a6e9b9bc18d5a656133e14d68f22
MYCOMPUTER\kyahhokid <kyahhokid@me.com>
2024-04-20 21:47:17 +0900

    会話コンポーネントの場所を修正

--- Brave-man-Go-buy-some-yakisoba-bread/src/app/app.module.ts
+++ Brave-man-Go-buy-some-yakisoba-bread/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConversationComponent } from './ui/game-area/components/conversation/conversation.component';
-import { NameComponent } from './ui/game-area/components/conversation/components/message-box/components/name/name.component';
-import { ContentOfConversationComponent } from './ui/game-area/components/conversation/components/message-box/components/content-of-conversation/content-of-conversation.component';
+import { ConversationComponent } from './ui/global/conversation/conversation.component';
+import { NameComponent } from './ui/global/conversation/components/message-box/components/name/name.component';
+import { ContentOfConversationComponent } from './ui/global/conversation/components/message-box/components/content-of-conversation/content-of-conversation.component';
 import { GameAreaComponent } from './ui/game-area/game-area.component';
-import { MessageBoxComponent } from './ui/game-area/components/conversation/components/message-box/message-box.component';
+import { MessageBoxComponent } from './ui/global/conversation/components/message-box/message-box.component';
 import { FamiconStyleGameComponent } from './ui/global/famicon-style-game/famicon-style-game.component';
 import { FamiconStyleMapComponent } from './ui/global/famicon-style-game/components/map/famicon-style-map.component';
 import { FamiconStyleMapBackgroundComponent } from './ui/global/famicon-style-game/components/map/components/background/famicon-style-map-background.component';
