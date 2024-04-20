b1c75405bd1e0dca6eacb2c832c8361e37ba8943
MYCOMPUTER\kyahhokid <kyahhokid@me.com>
2024-04-20 14:42:37 +0900

    キラ画像を表示する処理を実装

--- Brave-man-Go-buy-some-yakisoba-bread/src/app/ui/global/kira/kira.component.ts
+++ Brave-man-Go-buy-some-yakisoba-bread/src/app/ui/global/kira/kira.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, interval, takeUntil } from 'rxjs';
 
+/**
+ * キラコンポーネント
+ */
 @Component({
   selector: 'app-kira',
   templateUrl: './kira.component.html',
   styleUrls: ['./kira.component.scss'],
 })
-export class KiraComponent {
+export class KiraComponent implements OnInit, OnDestroy {
+  // キラ画像のソースパス
+  src = '';
+  // 表示中のキラ画像のインデックス
+  index = 0;
+  // キラ画像の動きを止めるサブジェクト
+  kiraStopSubject = new Subject()
 
+  ngOnInit(): void {
+    this.startKira();
+  }
+
+  ngOnDestroy(): void {
+    this.kiraStopSubject.next(null);
+  }
+
+  /**
+   * キラ画像の表示を始める。
+   */
+  private startKira() {
+    this.src = 'assets/images/kira0.png';
+    interval(100).pipe(takeUntil(this.kiraStopSubject)).subscribe(
+      () => {
+        this.index++;
+        if (this.index > 8) {
+          this.index = 0;
+        }
+        this.src = `assets/images/kira/kira${this.index}.png`
+      }
+    );
+  }
 }
